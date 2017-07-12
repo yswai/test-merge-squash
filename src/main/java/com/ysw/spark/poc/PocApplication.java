469c32960de30e1f3ea5ab89158fe525b873f7fa
Yep Siong Wai <siongwai.yep@trovicor.com>
2017-07-12 15:20:45 +0800

    Initial

---
+++ src/main/java/com/ysw/spark/poc/PocApplication.java
@@ -0,0 +1,12 @@
+package com.ysw.spark.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocApplication.class, args);
+	}
+}
